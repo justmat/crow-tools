52fab0d24510169505a4869c0bab8bfe2a9e26e2
matthew daniel <matthew@matthews-MacBook-Pro.local>
2019-10-05 17:43:16 -0400

    adder -> offset

--- adder.lua
+++ adder.lua
@@ -1,14 +1,14 @@
 -- input 1 - pitch cv
 -- input 2 - octave cv
 
-local adder = 0.0
+local offset = 0.0
 
-local function set_adder(v)
-  adder = math.floor(v)
+local function set_offset(v)
+  offset = math.floor(v)
 end
 
 input[1].stream = function(volts) 
-  output[1].volts = input[1].volts + adder
+  output[1].volts = input[1].volts + offset
 end
 
 function init()
@@ -16,7 +16,7 @@ function init()
   input[2].mode('stream', 0.01)
 
   metro[1].event = function()
-    set_adder(input[2].volts)
+    set_offset(input[2].volts)
   end
   metro[1].time = 0.01
   metro[1]:start()
