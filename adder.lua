2f2c4de926a06acc97bef8fda1e306823e71f203
matthew daniel <matthew@matthews-MacBook-Pro.local>
2019-10-05 19:30:50 -0400

    use streams

--- adder.lua
+++ adder.lua
@@ -3,21 +3,15 @@
 
 local offset = 0.0
 
-local function set_offset(v)
-  offset = math.floor(v)
-end
-
 input[1].stream = function(volts) 
   output[1].volts = input[1].volts + offset
+end
+
+input[2].stream = function(volts)
+  offset = math.floor(volts)
 end
 
 function init()
   input[1].mode('stream', 0.01)
   input[2].mode('stream', 0.01)
-
-  metro[1].event = function()
-    set_offset(input[2].volts)
-  end
-  metro[1].time = 0.01
-  metro[1]:start()
 end
